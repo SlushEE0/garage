13aaaeabc045a4ec09df08bc2dbb4cb00fc32902
SlushEE0 <neeladem2009@gmail.com>
2023-04-25 18:13:19 -0700

    master

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,16 @@ import React from 'react';
 import Toast from 'react-hot-toast';
 
 export default function App({ UsersArr }) {
-  const storedUser = localStorage.getItem('userData');
+  const storedUser = localStorage.getItem('UserData');
 
   const [auth, SETauth] = React.useState(UsersArr.includes(storedUser));
   const [allowed, SETallowed] = React.useState(true);
-
-  const changeGarageState = async function () {
-    SETallowed(false);
-    if (!auth) window.location.reload();
-    if (!confirm('Open Garage?')) return;
-
-    try {
-      await fetch(
-        `https://garageopener-27000-default-rtdb.firebaseio.com/${userInfo}/state.json`,
-        {
-          method: 'PUT',
-          body: JSON.stringify(1)
-        }
-      );
-    } catch (err) {
-      Toast.err('Something went wrong');
-      console.err(err);
-    }
-
-    Toast.success('Opening / Closing Garage');
-
-    setTimeout(() => {
-      SETallowed(true);
-    }, 2000);
-  };
 
   if (!auth) {
     const [userInput, SETuserInput] = React.useState();
 
     if (userInput && UsersArr.includes(userInput)) {
-      localStorage.setItem('userData', userInput);
+      localStorage.setItem('UserData', userInput);
       window.location.reload();
     }
 
@@ -54,13 +29,50 @@ export default function App({ UsersArr }) {
       </main>
     );
   } else if (auth) {
+    const logout = function () {
+      localStorage.removeItem('UserData');
+      window.location.reload();
+    };
+
+    const changeGarageState = async function () {
+      SETallowed(false);
+      if (!auth) window.location.reload();
+      if (!confirm('Open Garage?')) return;
+
+      console.log('confimed');
+
+      try {
+        await fetch(
+          `https://garageopener-27000-default-rtdb.firebaseio.com/${storedUser}/state.json`,
+          {
+            method: 'PUT',
+            body: JSON.stringify(1)
+          }
+        );
+      } catch (err) {
+        Toast.err('Something went wrong');
+        console.err(err);
+      }
+
+      Toast.success('Opening / Closing Garage');
+
+      setTimeout(() => {
+        SETallowed(true);
+      }, 2000);
+    };
+
     return (
-      <button
-        className='openBtn'
-        onClick={changeGarageState}
-        disabled={!allowed}>
-        Click me to Open Garage
-      </button>
+      <>
+        <button
+          className='openBtn'
+          onClick={changeGarageState}
+          disabled={!allowed}>
+          Click me to Open Garage
+        </button>
+        <button className='logoutBtn' onClick={logout}>
+          Log Out
+        </button>
+      </>
     );
   } else {
     return <h1>Something went horribly wrong, refresh the page</h1>;
